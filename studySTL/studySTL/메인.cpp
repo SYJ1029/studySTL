810680d8c9520b1e76f1cc14476df57b762b39c4
SYJ1029 <abg086@gmail.com>
2025-03-31 17:40:48 +0900

    변환을 함수는 따로 있을 것이다.

--- studySTL/studySTL/메인.cpp
+++ studySTL/studySTL/메인.cpp
@@ -13,45 +13,16 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <algorithm>
-#include <filesystem>
-#include <memory>
 #include "save.h"
 
 //[문제] "메인.cpp" 파일의 소문자를 대문자로 변환하여 "메인 대문자.cpp"에 저장하라
-
-void Load(const std::string filename, std::string& str) {
-	std::ifstream in(filename.data());
-	
-	if (not in) {
-		std::cout << "can't open" << std::endl;
-	}
-
-	str.resize(std::filesystem::file_size(filename));
-	in.read((char*)str.data(), std::filesystem::file_size(filename));
-	
-
-
-}
-
-void SaveUpperCase(std::string str) {
-	std::ofstream out("메인 대문자.cpp");
-	std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) {return std::toupper(c); });
-	std::copy(str.begin(), str.end(), std::ostreambuf_iterator{ out });	// copy를 할 때 str.end()를 붙여야 했던 이유는 무엇일까?
-}
 
 
 //----------------
 int main()
 //----------------
 {
-	{
-		std::string str;
-		Load("메인.cpp", str);
-		SaveUpperCase(str);
-	}
-
 	std::ifstream in{ "메인.cpp" };
 	if (not in)
 		return 15578884;

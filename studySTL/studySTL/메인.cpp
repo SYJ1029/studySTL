09fa955a670406cf0ebedd3d93a672aa18dd64db
SYJ1029 <abg086@gmail.com>
2025-03-31 19:05:43 +0900

    진행상황 저장

--- studySTL/studySTL/메인.cpp
+++ studySTL/studySTL/메인.cpp
@@ -13,10 +13,11 @@
 
 #include <iostream>
 #include <array>
-#include <algorithm>
 #include <random>
 #include <print>
 #include <ranges>
+#include <algorithm>
+#include <chrono>
 #include "save.h"
 
 //[문제] 값의 범위가 [0, 1'000'0000)인 랜덤 int값 1000만개를 저장할 
@@ -30,18 +31,33 @@ std::uniform_int_distribution uid{ 0, 999'9999 };
 
 std::array<int, 1'000'0000> arr;
 
+
+bool 내림차순(int a, int b)
+{
+	return a > b; // greater 연산자일 경우 내림차순
+}
+
+
 //----------------
 int main()
 //----------------
 {
 	for (int& num : arr)
 		num = uid(dre);
-	// qsort로 정렬
+	// stl의 sort로 내림차순 정렬
 
-	//std::qsort((void*)arr.data(), arr.size(), sizeof(int), {});
+
+	//스톱워치 시작
+	auto b = std::chrono::high_resolution_clock::now();
+	std::sort(arr.begin(), arr.end(), 내림차순);		// 더 간단해졌다 -> 디폴트가 오름차순이다
+	auto e = std::chrono::high_resolution_clock::now();
+	// 스톱워치 끝
+	
+	std::cout << "정렬에 걸린 시간" << e - b << std::endl;	// 경과시간
 
 	int num = 0;
-	for (int num : arr | std::views::take(1000) | std::views::take(10))
+	for (int num : arr | std::views::take(1000))		// 확률적으로 uniform 하다(범위에 대해 골고루 퍼진 값)
 		std::print("{:8}", arr[num]);
+	std::cout << std::endl;
 	//save("메인.cpp");
 }

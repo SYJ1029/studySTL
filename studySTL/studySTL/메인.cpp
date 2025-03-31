c7344b7100420a60f96d26bcb7f99a23be355196
SYJ1029 <abg086@gmail.com>
2025-03-31 17:33:20 +0900

    이야 내 코드 개판인데?

--- studySTL/studySTL/메인.cpp
+++ studySTL/studySTL/메인.cpp
@@ -6,7 +6,8 @@
 
 
 /*-------------------------------------------------
-// Callable
+// Callabe types - 호출가능한 타입
+// 1. 함수
 --------------------------------------------------*/
 
 
@@ -49,10 +50,25 @@ void SaveUpperCase(std::string str) {
 int main()
 //----------------
 {
-	std::string str;
+	{
+		std::string str;
+		Load("메인.cpp", str);
+		SaveUpperCase(str);
+	}
 
-	Load("메인.cpp", str);
-	SaveUpperCase(str);
+	std::ifstream in{ "메인.cpp" };
+	if (not in)
+		return 15578884;
+
+	std::ofstream out{ "메인 대문자.cpp" };
+
+	char c;
+	while (in >> c) {
+		if (std::islower(c))
+			c = std::toupper(c);
+
+		out << c;
+	}
 	
 	//save("메인.cpp");
 }

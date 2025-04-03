7838d784453ace476b006cd39b28417fb0e39269
DESKTOP-HGMKATH\abg09 <abg0986@gmail.com>
2025-04-04 00:55:47 +0900

    파일 입력 성공

--- studySTL/MySTL_Lab/메인.cpp
+++ studySTL/MySTL_Lab/메인.cpp
@@ -1,94 +1,73 @@
 /*--------------------------------
-// 2025학년도 1학기 STL 월910수910
+// 2025학년도 1학기 STL 월910수910				(5주 1일)
 // 3월 17일 월요일
-// 4주 1일
+// 중간고사		- 8주 1일(4월 24일 목요일)
+// 과제설명(30)	- 6주 1일(4월 10일 목요일)
 ---------------------------------*/
 
 
 /*-------------------------------------------------
-// Callabe types - 호출가능한 타입
-// 1. 함수
+// Callabe types - 호출가능한 타입 ==> 모든 호출가능타입을 대표하는 function
 --------------------------------------------------*/
 
-
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <array>
-#include <random>
-#include <print>
 #include <ranges>
-#include <algorithm>
-#include <chrono>
 #include "save.h"
 
-//[문제] 값의 범위가 [0, 1'000'0000)인 랜덤 int값 1000만개를 저장할 
-// 메모리를 확보하고 값을 채워라
-// (시작) qsort로 오름차순 정렬하라
-// 정렬한 결과 중에서 앞에서 부터 1000개만 화면에 출력해라
 
+// [상황] e-class에서 다운받은 파일 "Dog 십만마리"에는 
+// 정확하게 Dog 객체 10'0000마리가 저장되어 있다
+// 파일은
+//			ofstream out("Dog 10만마리");
+// Dog 타입의 객체 dog는
+//			out << dog;
+// 코드로 저장하였다
 
-std::default_random_engine dre;
-std::uniform_int_distribution uid{ 0, 999'9999 };
+class Dog {
+private:
+	size_t num;					// 랜덤 int 값
+	std::string name;			// [3, 60) 까지의 랜덤 소문자로만 구성
 
-std::array<int, 1'000'0000> arr;
-std::array<int, 1'000'0000> arrq;
+	friend std::ostream& operator<<(std::ostream& os, const Dog& dog) {
+		return os << dog.num << " " << dog.name << " ";
+	}
 
+	friend std::istream& operator>>(std::istream& is, Dog& dog) {
+		return is >> dog.num >> dog.name;
+	}
+};
 
-bool 내림차순(int a, int b)
-{
-	return a > b; // greater 연산자일 경우 내림차순
-}
+// [문제] 파일에 저장된 10만마리의 Dog객체를 모두 읽어 메모리에 저장하라
+// 마지막 객체의 정보를 화면에 출력하고 출력된 내용을 답지에도 적어라
+// 메모리에 있는 Dog객체를 name 길이 기준 오름차순으로 정렬하라
+// 정렬한 마지막 객체의 정보를 화면에 출력하고 답지에도 적어라
 
-bool 오름차순(int a, int b)
-{
-	return a < b;	// less 연산자일 경우 오름차순
-}
+std::array<Dog, 10'0000> arr;
 
-int 오름차순q(const void* a, const void* b)
-{
-
-	return *(int*)a - *(int*)b;
-}
-
-int 내림차순q(const void* a, const void* b)
-{
-	return *(int*)a - *(int*)b;
-}
 
 //----------------
 int main()
 //----------------
 {
-	for (int& num : arr)
-		num = uid(dre);
-	for (int& numq : arrq)
-		numq = uid(dre);
-	// stl의 sort로 내림차순 정렬
 
-	int(*정렬기준)(const void*, const void*) = 오름차순q;
+	std::ifstream in("Dog 십만마리");
 
-
-	//스톱워치 시작
-	auto b = std::chrono::high_resolution_clock::now();
-	std::sort(arr.begin(), arr.end(), std::greater<int>()); // 더 간단해졌다 -> 디폴트가 오름차순이다
-	auto e = std::chrono::high_resolution_clock::now();
-
-	// 스톱워치 끝
-
-	std::cout << "sort 정렬에 걸린 시간" << e - b << std::endl << std::endl;	// 경과시간
+	if (not in) {
+		std::cout << "cant open :(" << std::endl;
+		exit(15578884);
+	}
 
 
-	//2차 스톱워치 시작
-	auto b2 = std::chrono::high_resolution_clock::now();
-	std::qsort(arrq.data(), arrq.size(), sizeof(int), 정렬기준);
-	auto e2 = std::chrono::high_resolution_clock::now();
-	//2차 스톱워치 종료
-
-	std::cout << "qsort 정렬에 걸린 시간" << e2 - b2 << std::endl << std::endl;	// 경과시간
+	for (Dog& dog : arr) {
+		in >> dog;
+	}
+	
 
 
-	int num = 0;
-	for (int num : arr | std::views::take(1000))		// 확률적으로 uniform 하다(범위에 대해 골고루 퍼진 값)
-		std::print("{:8}", num);
-	std::cout << std::endl;
+	std::cout << arr.back() << std::endl;
+
 	//save("메인.cpp");
 }
